aa4501143c08f6670d66256d64d0360708a93ac1
@jpvalery <jpvalery@users.noreply.github.com>
2021-09-24 17:14:54 -0400

    feat: prettier + dotclub theme

--- api/_lib/template.ts
+++ api/_lib/template.ts
@@ -1,34 +1,48 @@
-
-import { readFileSync } from 'fs';
-import marked from 'marked';
-import { sanitizeHtml } from './sanitizer';
-import { ParsedRequest } from './types';
-const twemoji = require('twemoji');
-const twOptions = { folder: 'svg', ext: '.svg' };
+import { readFileSync } from "fs";
+import marked from "marked";
+import { sanitizeHtml } from "./sanitizer";
+import { ParsedRequest } from "./types";
+const twemoji = require("twemoji");
+const twOptions = { folder: "svg", ext: ".svg" };
 const emojify = (text: string) => twemoji.parse(text, twOptions);
 
-const rglr = readFileSync(`${__dirname}/../_fonts/Inter-Regular.woff2`).toString('base64');
-const bold = readFileSync(`${__dirname}/../_fonts/Inter-Bold.woff2`).toString('base64');
-const mono = readFileSync(`${__dirname}/../_fonts/Vera-Mono.woff2`).toString('base64');
-const srfrglr = readFileSync(`${__dirname}/../_fonts/Merriweather-Regular.woff2`).toString('base64');
-const srfbold = readFileSync(`${__dirname}/../_fonts/Merriweather-Bold.woff2`).toString('base64');
+const rglr = readFileSync(
+  `${__dirname}/../_fonts/Inter-Regular.woff2`
+).toString("base64");
+const bold = readFileSync(`${__dirname}/../_fonts/Inter-Bold.woff2`).toString(
+  "base64"
+);
+const mono = readFileSync(`${__dirname}/../_fonts/Vera-Mono.woff2`).toString(
+  "base64"
+);
+const srfrglr = readFileSync(
+  `${__dirname}/../_fonts/Merriweather-Regular.woff2`
+).toString("base64");
+const srfbold = readFileSync(
+  `${__dirname}/../_fonts/Merriweather-Bold.woff2`
+).toString("base64");
 
 function getCss(theme: string, fontSize: string) {
-    let background = '#111827';
-    let foreground = 'white';
+  let background = "#111827";
+  let foreground = "white";
 
-    if (theme === 'dotphoto') {
-        background = '#16161d';
-        foreground = '#e88b6a';
-    }
+  if (theme === "dotphoto") {
+    background = "#16161d";
+    foreground = "#e88b6a";
+  }
 
-    let fontFamily = 'Inter';
+  if (theme === "dotclub") {
+    background = "white";
+    foreground = "black";
+  }
 
-    if (theme === 'dotphoto') {
-        fontFamily = 'Merriweather'
-    }
+  let fontFamily = "Inter";
 
-    return `
+  if (theme === "dotphoto") {
+    fontFamily = "Merriweather";
+  }
+
+  return `
     @font-face {
         font-family: 'Inter';
         font-style:  normal;
@@ -123,8 +137,8 @@ function getCss(theme: string, fontSize: string) {
 }
 
 export function getHtml(parsedReq: ParsedRequest) {
-    const { text, theme, md, fontSize, images, widths, heights } = parsedReq;
-    return `<!DOCTYPE html>
+  const { text, theme, md, fontSize, images, widths, heights } = parsedReq;
+  return `<!DOCTYPE html>
 <html>
     <meta charset="utf-8">
     <title>Generated Image</title>
@@ -136,13 +150,16 @@ export function getHtml(parsedReq: ParsedRequest) {
         <div>
             <div class="spacer">
             <div class="logo-wrapper">
-                ${images.map((img, i) =>
-                    getPlusSign(i) + getImage(img, widths[i], heights[i])
-                ).join('')}
+                ${images
+                  .map(
+                    (img, i) =>
+                      getPlusSign(i) + getImage(img, widths[i], heights[i])
+                  )
+                  .join("")}
             </div>
             <div class="spacer">
             <div class="heading">${emojify(
-                md ? marked(text) : sanitizeHtml(text)
+              md ? marked(text) : sanitizeHtml(text)
             )}
             </div>
         </div>
@@ -150,16 +167,16 @@ export function getHtml(parsedReq: ParsedRequest) {
 </html>`;
 }
 
-function getImage(src: string, width ='auto', height = '225') {
-    return `<img
+function getImage(src: string, width = "auto", height = "225") {
+  return `<img
         class="logo"
         alt="Generated Image"
         src="${sanitizeHtml(src)}"
         width="${sanitizeHtml(width)}"
         height="${sanitizeHtml(height)}"
-    />`
+    />`;
 }
 
 function getPlusSign(i: number) {
-    return i === 0 ? '' : '<div class="plus">+</div>';
+  return i === 0 ? "" : '<div class="plus">+</div>';
 }
--- api/_lib/types.ts
+++ api/_lib/types.ts
@@ -1,13 +1,13 @@
-export type FileType = 'png' | 'jpeg';
-export type Theme = 'dotme' | 'dotphoto';
+export type FileType = "png" | "jpeg";
+export type Theme = "dotme" | "dotphoto" | "dotclub";
 
 export interface ParsedRequest {
-    fileType: FileType;
-    text: string;
-    theme: Theme;
-    md: boolean;
-    fontSize: string;
-    images: string[];
-    widths: string[];
-    heights: string[];
+  fileType: FileType;
+  text: string;
+  theme: Theme;
+  md: boolean;
+  fontSize: string;
+  images: string[];
+  widths: string[];
+  heights: string[];
 }
