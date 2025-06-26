73a49d8cb44551d1af562a4b1541408f9792fcd1
jpvalery <jpvalery@users.noreply.github.com>
2025-06-26 17:15:26 -0400

    fix: type error

--- app/api/og/flaps/[slug]/route.tsx
+++ app/api/og/flaps/[slug]/route.tsx
@@ -92,8 +92,8 @@ export async function GET(
         ],
       },
     );
-  } catch (e: unknown) {
-    console.log(e.message);
+  } catch (e: any) {
+    console.log(`${e.message}`);
     return new Response("Failed to generate the image", {
       status: 500,
     });
